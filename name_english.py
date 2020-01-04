8fe8afe81fb35bcfa809d244b6020a62d0841576
sandeep jain <41137947+hellosandeep1999@users.noreply.github.com>
2020-01-04 19:37:02 +0530

    Add files via upload

---
+++ name_english.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan  4 15:01:35 2020
+
+@author: user
+"""
+
+#Name printing in english
+f_name = input("Enter First name: ")
+l_name = input("Enter Last name: ")
+print(f_name,"\n",l_name)
