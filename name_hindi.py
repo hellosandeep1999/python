bb62c3dc95e2c38301b163e910198f707f06c38a
sandeep jain <41137947+hellosandeep1999@users.noreply.github.com>
2020-01-04 19:40:40 +0530

    Add files via upload

---
+++ name_hindi.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan  4 16:16:56 2020
+
+@author: user
+"""
+
+#name printing in hindi
+print("\u0938\u093F\u0932\u094D\u0935\u0947\u0938\u094D\u091F\u0930, ","\u092B\u0930\u094D\u0928\u093E\u0902\u0921\u0940\u091C")
