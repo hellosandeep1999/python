b22b570e41e4bf3b75f041c5a345e57014aef031
sandeepjain20178 <sandeepjain20178@gmail.com>
2020-01-05 17:57:03 +0530

    Added ne file

---
+++ factorial.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan  4 18:27:56 2020
+
+@author: user
+"""
+
+#factorial
+
+import math as mt
+
+x = int(input("Enter a number: "))
+print(math.factorial(x))
+
+
+
+
+
+
+"""
+x = int(input("enter a number: "))
+
+fact = 1
+
+if x<0:
+    print("negative numbers are not allowed")
+elif x == 0:
+    print("factorial of zero is always zero")
+else:
+    for a in range(1,x+1):
+        fact = fact*a
+    print(fact)
+    
+    
+"""
