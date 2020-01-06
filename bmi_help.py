c4fc920da67e543d2665d2780f70b08c2fa4cecc
sandeepjain20178 <sandeepjain20178@gmail.com>
2020-01-06 17:34:42 +0530

    Added New File

---
+++ bmi_help.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan  4 16:50:07 2020
+
+@author: user
+"""
+
+#BMI help screen
+
+
+print("""
+          World Health Organisation's BMI VALUES ( 8 Levels )
+      Severe Thinness: less than 16 
+      Moderate Thinness: between 16 and 16.9 
+      Mild Thinness: between 17 and 18.4 
+      Normal: between 18.5 and 24.9 
+      Overweight: between 25 and 29.9        
+      Obese Class I: between 30 and 34.9 
+      Obese Class II: between 35 and 39.9 
+      Obese Class III: 40 or greater 
+      """)
