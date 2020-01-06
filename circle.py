9f387dbba1265d1f259abf4298b0ea7da21b7824
sandeepjain20178 <sandeepjain20178@gmail.com>
2020-01-06 17:29:25 +0530

    Added new file in

---
+++ circle.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan  4 18:52:59 2020
+
+@author: user
+"""
+
+
+
+#Area and Perimeter of Circle
+
+
+import math
+
+r = int(input("Enter the raidus of a circle"))
+
+area = math.pi * r * r
+parameter = 2 * math.pi * r 
+
+print("area of circle = ",area )
+print("parameter of circle = ",parameter) 
