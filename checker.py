b8dfd91d402b23191ad31dd208362d0ee65ea889
sandeepjain20178 <sandeepjain20178@gmail.com>
2020-01-07 19:02:46 +0530

    Added new file

---
+++ checker.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan  4 16:42:11 2020
+
+@author: user
+"""
+
+#Name printing checkerboard Pattern
+print("* * * * * * * *")
+print(" * * * * * * * *")
+print("* * * * * * * *")
+print(" * * * * * * * *")
+print("* * * * * * * *")
+print(" * * * * * * * *")
+print("* * * * * * * *")
