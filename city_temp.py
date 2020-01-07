2658c7ad48e4076110a1a8380249127261c87c5a
sandeepjain20178 <sandeepjain20178@gmail.com>
2020-01-07 19:37:50 +0530

    Added new file

---
+++ city_temp.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan  4 15:33:59 2020
+
+@author: user
+"""
+
+#Temperature of city
+
+temp = int(input("Enter the temperature of your city: "))
+print(temp,"\u00b0","C")
