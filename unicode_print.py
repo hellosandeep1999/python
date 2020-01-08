e2a2681f392bacda76fae765657c643fd56e3e74
sandeepjain20178 <sandeepjain20178@gmail.com>
2020-01-08 19:54:19 +0530

    Added new File

---
+++ unicode_print.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan  4 15:43:49 2020
+
+@author: user
+"""
+
+#Unicode string
+print("UNIX","\u00AE","and Sun Microsystem","\u00A9","are Copyright","\u2122,", "2019 Oracle")
