3be6e824bb88569ee19b8fccdbc4c930cdd2922b
sandeepjain20178 <sandeepjain20178@gmail.com>
2020-01-09 18:46:07 +0530

    Added new file

--- hands_on1.py
+++ hands_on1.py
@@ -72,6 +72,29 @@ print(res)
 
 
 
+# Given strings a and b, return a single string with a and b separated
+# by a space '<a> <b>', except swap the first 2 chars of each string.
+# e.g.
+#   'mix', pod' -> 'pox mid'
+#   'dog', 'dinner' -> 'dig donner'
+# Assume a and b are length 2 or more.
+
+
+
+a = input("Enter first string")
+b = input("Enter second string")
+print(b[0:2]+a[2:],"",a[0:2]+b[2:])
+
+
+
+
+
+
+
+
+
+
+
 
 
 
