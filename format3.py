da65f3c3b91d84269da2bcfc1713cb0cfaf1cf8a
sandeepjain20178 <sandeepjain20178@gmail.com>
2020-01-11 18:11:34 +0530

    Added new file

---
+++ format3.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 11 17:14:06 2020
+
+@author: user
+"""
+
---
+++ style.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 10 18:07:55 2020
+
+@author: user
+"""
+
+#Styling of string
+
+
+inp = input("Enter a name: ")
+a = inp.upper()
+b = inp.lower()
+c = inp.title()
+print(" Uppercase:", a,"\n","Lowercase:",b,"\n","Title: ",c)
+
