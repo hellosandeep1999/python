03eb58bfc641054f4c3435b18e38906cbabbc871
sandeepjain20178 <sandeepjain20178@gmail.com>
2020-01-13 16:53:52 +0530

    Added new file

---
+++ age_cal.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 13 16:41:06 2020
+
+@author: user
+"""
+
+#age calculater
+
+age = int(input("What is your present age"))
+if age>=10:
+    print("After 5 year",age+5,"\n before 10 year back",age-10)
+else:
+    print("age must be 10 or greater than 10")
