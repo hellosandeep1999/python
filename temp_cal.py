94040d4f237dfb157bde5937e27649a4b98fc305
sandeepjain20178 <sandeepjain20178@gmail.com>
2020-01-18 19:47:31 +0530

    Added new file

---
+++ temp_cal.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 13 19:44:01 2020
+
+@author: user
+"""
+
+
+
+#Temperature Calculator 
+
+
+
+inp = float(input("Enter today's temprature(in degree) of your city: "))
+F_deg = (inp*1.8)+35
+K_deg = inp+273
+print("The temprature in Degree Fahrenheit: ",F_deg,"\nThe temprature in Degree kelvin: ",K_deg)
+
+
+
