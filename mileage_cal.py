5da83d0e3c5412541b41004985ee0dfac2b351ef
sandeepjain20178 <sandeepjain20178@gmail.com>
2020-01-20 00:13:04 +0530

    Added new file

---
+++ mileage_cal.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 15 10:57:29 2020
+
+@author: user
+"""
+
+
+# Gas Mileage Calculator 
+
+
+km_car,ptrl_car = float(input("Enter total kilometer: ")),float(input("Enter putting patrol(in leter): "))
+print("Mileage of your car: ",km_car/ptrl_car,"Km/li")
+
+
+
+
+
