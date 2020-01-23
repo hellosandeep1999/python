7e5c6ff60ce6ec155abe95131fe82cbddbea9aa1
sandeepjain20178 <sandeepjain20178@gmail.com>
2020-01-23 15:14:52 +0530

    Added new file

---
+++ pattern.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 22 16:23:48 2020
+
+@author: user
+"""
+
+#Pattern builder
+
+
+n = int(input("Enter a number of n: "))
+for i in range(n):
+    print("* "*(i+1))
+for j in range(n):
+    print("* "*(n-j-1))
+    
+    
+    
+    
+    
+    
+    
+    
+    
