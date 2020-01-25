5d92e46ce2184a4cd2a777cf5dbf7f4ed533babb
sandeepjain20178 <sandeepjain20178@gmail.com>
2020-01-25 14:54:55 +0530

    Added new file

---
+++ sorting.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 23 15:13:28 2020
+
+@author: user
+"""
+
+a = [("tom",17,90),("john",17,90),("jonson",17,90),("jony",17,90)]
+a.sort()
+print(a)
+    
