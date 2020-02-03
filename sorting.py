8d6b99a5077801d9cfce66fffccdfd5d5c4a937c
sandeepjain20178 <sandeepjain20178@gmail.com>
2020-02-03 18:30:02 +0530

    Added new file

--- sorting.py
+++ sorting.py
@@ -1,11 +1,16 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Jan 23 15:13:28 2020
+Created on Wed Jan 29 15:06:04 2020
 
 @author: user
 """
+n = int(input("Enter number of person:"))
+b = []
+for i in range(n):
+    a = input("Enter name, age , height: ").split(',')
+    g = [a[0],int(a[1]),int(a[2])]
+    c = tuple(g)
+    b.append(c)
+b.sort()
+print(b)
 
-a = [("tom",17,90),("john",17,90),("jonson",17,90),("jony",17,90)]
-a.sort()
-print(a)
-    
