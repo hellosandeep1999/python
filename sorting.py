3eb491b7d924c480b7a02c02118c25938cd6fc49
Sandeep Jain <41137947+hellosandeep1999@users.noreply.github.com>
2020-05-04 13:33:37 +0530

    Update sorting.py

--- sorting.py
+++ sorting.py
@@ -4,6 +4,38 @@ Created on Wed Jan 29 15:06:04 2020
 
 @author: user
 """
+"""
+Name: 
+    Sorting         
+Filename:
+    sorting.py
+Problem Statement:
+    You are required to write a program to sort the (name, age, height) 
+    tuples by ascending order where name is string, age and height are numbers. 
+    The tuples are input by console. The sort criteria is:
+    1: Sort based on name;
+    2: Then sort based on age;
+    3: Then sort by score. 
+    The priority is that name > age > score 
+Data:
+    Not required
+Extension:
+    Aces can be 1 or 11! The number used is whichever gets the highest score.  
+Sample Input:
+    Tom,19,80
+    John,20,90
+    Jony,17,91
+    Jony,17,93
+    Json,21,85
+Sample Output:
+    [('John', 20, 90), ('Jony', 17, 91), ('Jony', 17, 93), ('Json', 21, 85), ('Tom', 19, 80)]
+""" 
+
+
+
+
+
+
 n = int(input("Enter number of person:"))
 b = []
 for i in range(n):
