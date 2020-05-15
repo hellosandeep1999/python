0bc034113d704a3b6cfbd0b182adbb433ccf87e5
sandeepjain20178 <sandeepjain20178@gmail.com>
2020-05-15 09:43:02 +0530

    Added new file

--- zoo.py
+++ zoo.py
@@ -20,66 +20,26 @@ Code Challenge
     print the total number of water needed by all the animals    
 """
 
-
 import csv
-water_elephant=0
-water_tiger=0
-water_lion=0
-water_zebra=0
-water_kangaroo=0
-counter=0
-counter1=0
-counter2=0
-counter3=0
-counter4=0
-with open("zoo.csv") as zoo:
-        zoo_reader = csv.DictReader(zoo)
 
-        for row in zoo_reader:
-               
-              #print(row['animal'])
-             if row['animal']=='elephant':
-                        if(counter==0):
-                             print("Elephant :")
-                             print(row['animal'],row['uniq_id'],row['water_need'] )
-                             water_elephant+=int(row['water_need'])
-                             counter+=1
-
-             elif row['animal']=='tiger':
-
-                        if(counter1==0):
-                             print("Tiger :")
-                             print(row['animal'],row['uniq_id'],row['water_need'] )
-                             water_tiger+=int(row['water_need'])
-                             counter1+=1
-
-             elif row['animal']=='lion':
-                         if(counter2==0):
-                             print("Lion :")
-                             print(row['animal'],row['uniq_id'],row['water_need'] )
-                             water_lion+=int(row['water_need'])
-                             counter2+=1
-
-             elif row['animal']=='zebra':
-                         if(counter3==0):
-                            print("Zebra :")
-                            print(row['animal'],row['uniq_id'],row['water_need'] )
-                            water_zebra+=int(row['water_need'])
-                            counter3+=1
-
-             elif row['animal']=='kangaroo':
-                         if(counter4==0):
-                            print("Kangaroo :")
-                            print(row['animal'],row['uniq_id'],row['water_need'] )
-                            water_kangaroo+=int(row['water_need'])
-                            counter4+=1
-
-        print("Water needed for elephant :",water_elephant)
-        print("Water needed for tiger :",water_tiger)
-        print("Water needed for lion :",water_lion)
-        print("Water needed for zebra :",water_zebra)
-        print("Water needed for kangaroo :",water_kangaroo)
-        total=0
-        total=water_elephant + water_lion + water_tiger + water_zebra + water_kangaroo
-        print("Total water consume by animal",total)
+list1 = []
+dict = {}
+with open('zoo.csv') as file1:
+    file = file1.readlines()
+    for row in file[1:]:
+        line = row.strip().split(',')
+        if line[0] not in list1:
+            list1.append(line[0])
+        if line[0] not in dict:
+            dict[line[0]] = int(line[2])
+        else:
+            dict[line[0]] += int(line[2])
+            
+            
+print("animals: ",list1)
+total_water = 0
+for name,water in dict.items():
+    print(name,"-->",water)
+    total_water += water
+print("Total water",total_water)
 
